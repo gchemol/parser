158e28c27af38443deaa22df1afe260308974475
Wenping Guo <ybyygu@gmail.com>
2020-01-10 17:19:39 +0800

    remove adhoc marker

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,6 @@
 // [[file:~/Workspace/Programming/gchemol-rs/parser/parser.note::*mods][mods:1]]
 mod core;
 mod reader;
-
-#[cfg(feature = "adhoc")]
 pub mod parsers;
 // mods:1 ends here
 
@@ -12,7 +10,5 @@ pub mod parsers;
 
 // [[file:~/Workspace/Programming/gchemol-rs/parser/parser.note::*re-exports][re-exports:1]]
 pub use crate::core::*;
-
-#[cfg(feature = "adhoc")]
 pub use crate::reader::*;
 // re-exports:1 ends here
