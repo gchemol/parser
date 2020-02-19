71201a318dba4aa9c28c81ce284540b4221189c2
Wenping Guo <ybyygu@gmail.com>
2020-02-19 17:25:57 +0800

    add TextParser for capability issue

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,14 @@
 // [[file:~/Workspace/Programming/gchemol-rs/parser/parser.note::*mods][mods:1]]
 mod core;
 mod reader;
-pub mod parsers;
+mod parser;
 // mods:1 ends here
 
 // exports
 
 // [[file:~/Workspace/Programming/gchemol-rs/parser/parser.note::*exports][exports:1]]
+pub mod parsers;
 pub use crate::reader::*;
+
+pub use crate::parser::TextParser;
 // exports:1 ends here
