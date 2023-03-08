110f4c0a5c8491ae86888cd6b8ebf155372d922d
Wenping Guo <ybyygu@gmail.com>
2023-03-08 16:25:36 +0800

    Remove parse_fortran_float function from crate root

--- src/lib.rs
+++ src/lib.rs
@@ -37,38 +37,6 @@ mod reader;
 mod view;
 // 9b3ecbac ends here
 
-// [[file:../parser.note::838e8dea][838e8dea]]
-/// Convert a string to a float.
-///
-/// This method performs certain checks, that are specific to quantum
-/// chemistry output, including avoiding the problem with Ds instead
-/// of Es in scientific notation. Another point is converting string
-/// signifying numerical problems (*****) to something we can manage
-/// (NaN).
-pub fn parse_fortran_float(s: &str) -> Option<f64> {
-    if s.chars().all(|x| x == '*') {
-        std::f64::NAN.into()
-    } else {
-        s.parse().ok().or_else(|| s.replacen("D", "E", 1).parse().ok())
-    }
-}
-
-#[test]
-fn test_fortran_float() {
-    let x = parse_fortran_float("14");
-    assert_eq!(x, Some(14.0));
-
-    let x = parse_fortran_float("14.12E4");
-    assert_eq!(x, Some(14.12E4));
-
-    let x = parse_fortran_float("14.12D4");
-    assert_eq!(x, Some(14.12E4));
-
-    let x = parse_fortran_float("****");
-    assert!(x.unwrap().is_nan());
-}
-// 838e8dea ends here
-
 // [[file:../parser.note::ff35c905][ff35c905]]
 pub mod parsers;
 pub mod partition;
